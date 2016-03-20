f133683acd6a8d6af3a21fa2a9dcfc62336b7cb7
Anand Kacha <ndkcha@gmail.com>
2016-03-20 21:29:04 +0530

    Initialized with Fresh Template

--- src/background.js
+++
@@ -1,9 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function () {
-    chrome.app.window.create('public/index.html', {
-        'outerBounds': {
-            'width': 800,
-            'height': 600
-        },
-        state: "maximized"        
-    });
-});
