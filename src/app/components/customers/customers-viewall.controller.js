022a3ea683b06b01333f837a6613a6209f4f26c0
Anand Kacha <ndkcha@gmail.com>
2016-04-21 10:08:46 +0530

    Search Query in Database for Customers Module

--- src/app/components/customers/customers-viewall.controller.js
+++ src/app/components/customers/customers-viewall.controller.js
@@ -11,9 +11,9 @@
     angular.module('automintApp')
         .controller('amCtrlCuRA', CustomersViewAll);
 
-    CustomersViewAll.$inject = ['$state', 'utils', 'amCustomers'];
+    CustomersViewAll.$inject = ['$scope', '$state', 'utils', 'amCustomers'];
 
-    function CustomersViewAll($state, utils, amCustomers) {
+    function CustomersViewAll($scope, $state, utils, amCustomers) {
         //  initialize view model
         var vm = this;
         
@@ -25,20 +25,64 @@
             total: 0
         };
         vm.customers = [];
+        vm.customerSearch = '';
+        vm.showPaginationBar = true;
+        vm.isQueryMode = false;
         
         //  function-maps
         vm.addCustomer = addCustomer;
         vm.getCustomers = getCustomers;
         vm.deleteCustomer = deleteCustomer;
         vm.editCustomer = editCustomer;
+        vm.queryCustomers = queryCustomers;
+        vm.changeQueryMode = changeQueryMode;
         
         //  default execution steps
         getCustomers();
         
         //  function definitions
         
+        function changeQueryMode(bool) {
+            vm.isQueryMode = bool;
+            if (!vm.isQueryMode)
+                getCustomers();
+            else {
+                queryCustomers();
+                setTimeout(focusOnSearchbox, 300);
+            }
+                
+            function focusOnSearchbox() {
+                $('#am-query-search').focus();
+            }
+        }
+        
+        //  fetch queried customers from database
+        function queryCustomers() {
+            if (vm.customerSearch == '')
+                getCustomers();
+            else
+                getQueriedCustomers();
+        }
+        
+        function getQueriedCustomers() {
+            vm.showPaginationBar = false;
+            vm.promise = amCustomers.queryUser;
+            vm.promise(vm.query.page, vm.query.limit, vm.customerSearch).then(success).catch(failure);
+            
+            function success(res) {
+                vm.customers = res.customers;
+                vm.query.total = res.total;
+            }
+            
+            function failure(err) {
+                vm.customers = [];
+                vm.query.total = 0
+            }
+        }
+        
         //  fetch relevant customers from database
         function getCustomers() {
+            vm.showPaginationBar = true;
             vm.promise = amCustomers.getCustomers;
             vm.promise(vm.query.page, vm.query.limit).then(success).catch(failure);
             
--- src/app/components/customers/customers.factory.js
+++ src/app/components/customers/customers.factory.js
@@ -10,9 +10,9 @@
 (function() {
     angular.module('automintApp')
         .factory('amCustomers', CustomersFactory);
-    
-    CustomersFactory.$inject = ['$q', '$http', 'utils','pdbCommon', 'pdbCustomers', '$amRoot'];
-    
+
+    CustomersFactory.$inject = ['$q', '$http', 'utils', 'pdbCommon', 'pdbCustomers', '$amRoot'];
+
     // function CustomersFactory($q:angular.IQService, $http:angular.IHttpService, utils, pdbCommon, pdbCustomers, $amRoot) {
     function CustomersFactory($q, $http, utils, pdbCommon, pdbCustomers, $amRoot) {
         //  initialize factory variable and map functions
@@ -23,19 +23,122 @@
             addNewCustomer: addNewCustomer,
             deleteCustomer: deleteCustomer,
             getCustomer: getCustomer,
-            saveCustomer: saveCustomer
+            saveCustomer: saveCustomer,
+            queryUser: queryUser
         };
-        
+
         return factory;
-        
+
         //  function definitions
-        
-        
+
+        // test function
+        function queryUser(page, limit, query) {
+            var tracker = $q.defer();
+
+            pdbCustomers.query({
+                map: mapQuery,
+                reduce: reduceQuery
+            }, {
+                group: true,
+                limit: limit,
+                skip: --page * limit
+            }).then(success).catch(failure);
+            
+            return tracker.promise;
+
+            function success(res) {
+                var result = [];
+                res.rows.forEach(iterateRows);
+                console.log(result);
+                tracker.resolve({
+                    total: result.length,
+                    customers: result
+                });
+
+                function iterateRows(row) {
+                    if (row.value)
+                        row.value.forEach(iterateValues);
+                }
+
+                function iterateValues(value) {
+                    var customer = {},
+                        vehicles = [];
+                    if (value) {
+                        customer.id = value._id;
+                        customer.name = value.name;
+                        customer.mobile = value.mobile;
+                        customer.email = value.email;
+                        if (value.vehicles) {
+                            Object.keys(value.vehicles).forEach(iterateVehicle);
+                            customer.vehicles = vehicles;
+                        }
+                        result.push(customer);
+                    }
+                    //  self cleaning
+                    delete customer;
+                    delete vehicles;
+
+                    function iterateVehicle(vId) {
+                        vehicles.push({
+                            reg: value.vehicles[vId].reg,
+                            manuf: value.vehicles[vId].manuf,
+                            model: value.vehicles[vId].model
+                        });
+                    }
+                }
+            }
+
+            function failure(err) {
+                tracker.reject({
+                    total: 0,
+                    customers: []
+                });
+                console.log(err);
+            }
+
+            function mapQuery(doc, emit) {
+                if (doc.user.vehicles)
+                    Object.keys(doc.user.vehicles).forEach(iterateVehicles);
+                doc.user._id = doc._id;
+                emit(query, doc.user);
+
+                function iterateVehicles(vehicle) {
+                    delete vehicle.services;
+                }
+            }
+
+            function reduceQuery(keys, values, rereduce) {
+                var result = [];
+                var q = angular.lowercase(keys[0][0]);
+                values.forEach(iterateValues);
+                
+                function iterateValues(value) {
+                    var vehicleFound = false;
+                    if (value.vehicles)
+                        Object.keys(value.vehicles).forEach(iterateVehicles);
+                    if ((value.name && angular.lowercase(value.name).search(q) > -1)
+                        || (value.email && angular.lowercase(value.email).search(q) > -1) 
+                        || (value.mobile && angular.lowercase(value.mobile).search(q) > -1) 
+                        || vehicleFound) {
+                        result.push(value);
+                    }
+                    
+                    function iterateVehicles(vId) {
+                        if ((value.vehicles[vId].manuf && angular.lowercase(value.vehicles[vId].manuf).search(q) > -1) 
+                            || (value.vehicles[vId].model && angular.lowercase(value.vehicles[vId].model).search(q) > -1) 
+                            || (value.vehicles[vId].reg && angular.lowercase(value.vehicles[vId].reg).search(q) > -1))
+                            vehicleFound = true;
+                    }
+                }
+                return result;
+            }
+        }
+
         //  get customers for datatable
         function getCustomers(page, limit) {
             var tracker = $q.defer();
             var customers = [];
-            var skip = --page*limit;
+            var skip = --page * limit;
             var dbOptions = {
                 include_docs: true,
                 limit: limit,
@@ -43,14 +146,15 @@
             }
             pdbCustomers.getAll(dbOptions).then(success).catch(failure);
             return tracker.promise;
-            
+
             //  if documents are accisible in database
             function success(res) {
                 if (res.rows.length > 0) {
                     res.rows.forEach(iterateRow);
-                    
+
                     function iterateRow(row) {
-                        var customer = {}, vehicles = [];
+                        var customer = {},
+                            vehicles = [];
                         if (row.doc && row.doc.user) {
                             customer.id = row.doc._id;
                             customer.name = row.doc.user.name;
@@ -65,7 +169,7 @@
                         //  self cleaning
                         delete customer;
                         delete vehicles;
-                        
+
                         function iterateVehicle(vId) {
                             vehicles.push({
                                 reg: row.doc.user.vehicles[vId].reg,
@@ -88,118 +192,118 @@
                 });
             }
         }
-        
+
         //  get all manufacturers available in Automint
         function getManufacturers() {
             var tracker = $q.defer();
             var manufacturers = [];
             pdbCommon.get('manuf-models').then(success).catch(missDb);
             return tracker.promise;
-            
+
             //  success ? add manufacturers to an array and return it via promise
             function success(response) {
                 Object.keys(response).forEach(manufIterator);
                 tracker.resolve(manufacturers);
             }
-            
+
             //  !success ? get manufacturer list from raw json file, then execute success sequence again
             function missDb(error) {
                 $http.get('data/manuf_model.json').success(success).catch(failure);
             }
-            
+
             //  iterate through manufacturer list and get individual manufacturer
             function manufIterator(manuf) {
                 if (!manuf.match(/\b_id|\b_rev/i))
                     manufacturers.push(manuf);
             }
-            
+
             //  thorwn an error via promise
             function failure(error) {
                 tracker.reject(error);
             }
         }
-        
+
         //  get model for particular manufacturer
         function getModels(manufacturer) {
             var tracker = $q.defer();
             var models = [];
             pdbCommon.get('manuf-models').then(success).catch(missDb);
             return tracker.promise;
-            
+
             //  success ? add models to an array and return it via promise
             function success(response) {
                 if (response[manufacturer] && response[manufacturer].models)
                     models = Object.keys(response[manufacturer].models);
                 tracker.resolve(models);
             }
-            
+
             //  !success ? get manufacturer list from raw json file, then execute success sequence again
             function missDb(error) {
                 $http.get('data/manuf_model.json').success(success).catch(failure);
             }
-            
+
             //  throw an error via promise
             function failure(error) {
                 tracker.reject(error);
             }
         }
-        
+
         //  delete customer from database
         function deleteCustomer(cId) {
             var tracker = $q.defer();
             pdbCustomers.get(cId).then(userFound).catch(failure);
             return tracker.promise;
-            
+
             function userFound(res) {
                 res._deleted = true;
                 pdbCustomers.save(res).then(success).catch(failure);
             }
-            
+
             function success(res) {
                 tracker.resolve(res);
             }
-            
+
             function failure(error) {
                 tracker.reject(error);
             }
         }
-        
+
         //  save customer to database
         function saveCustomer(document) {
             var tracker = $q.defer();
             pdbCustomers.save(document).then(success).catch(failure);
             return tracker.promise;
-            
+
             function success(response) {
                 tracker.resolve(response);
             }
-            
+
             function failure(error) {
                 tracker.reject(error);
             }
         }
-        
+
         //  add new customer
         function addNewCustomer(customer, vehicle) {
             var prefixUser = 'user-' + angular.lowercase(customer.name).replace(' ', '-'),
                 prefixVehicle;
-            
-            if (vehicle) { 
+
+            if (vehicle) {
                 prefixVehicle = 'vehicle' + ((vehicle.manuf && vehicle.model) ? '-' + angular.lowercase(vehicle.manuf).replace(' ', '-') + '-' + angular.lowercase(vehicle.model).replace(' ', '-') : '');
                 customer.vehicles = {};
                 delete vehicle.id;
                 customer.vehicles[utils.generateUUID(prefixVehicle)] = vehicle;
             }
-            
+
             var doc = {
                 _id: utils.generateUUID(prefixUser),
                 creator: $amRoot.username,
                 user: customer
             }
-            
+
             return saveCustomer(doc);
         }
-        
+
         //  get customer from database
         function getCustomer(id) {
             return pdbCustomers.get(id);
