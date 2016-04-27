9cd97fdce071740b40f55aefa2b02e7e9f88bbdf
Viral Kacha <vrl.kacha@gmail.com>
2016-04-27 13:24:10 +0530

    structure change for gulp build process

---
+++ pkg/gulpfile.js
@@ -0,0 +1,136 @@
+var gulp = require('gulp');
+var del = require('del');
+var runSequence = require('run-sequence');
+var minifyInline = require('gulp-minify-inline');
+var size = require('gulp-size');
+var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var htmlmin = require('gulp-htmlmin');
+var mainBowerFiles = require('main-bower-files');
+var jsonminify = require('gulp-jsonminify');
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant');
+
+gulp.task('default', function () {
+    console.log('Gulp is up and running.');
+    runSequence(
+        'clean',
+        'root-dir-files',
+        'app-dir',
+        'assets-dir',
+        'bower_components',
+        'data-dir',
+        'node_modules'
+    );
+});
+
+gulp.task('clean', function () {
+    return del([
+    'app'
+  ]);
+});
+
+gulp.task('root-dir-files', function () {
+    gulp.src('../src/index.html')
+        .pipe(minifyInline())
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('app'));
+    gulp.src(['../src/*.json', '!../src/typings.json'])
+        .pipe(jsonminify())
+        .pipe(gulp.dest('app'));
+    gulp.src('../src/main.js')
+        .pipe(gulp.dest('app'));
+});
+
+gulp.task('app-dir', function () {
+    gulp.src('../src/app/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('app/app'));
+    gulp.src('../src/app/**/*.html')
+        .pipe(minifyInline())
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('app/app'));
+});
+
+gulp.task('assets-dir', function () {
+    gulp.src('../src/assets/css/*.css')
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('app/assets/css'));
+    gulp.src('../src/assets/img/*')
+        .pipe(imagemin({
+            progressive: true
+        }))
+        .pipe(gulp.dest('app/assets/img'));
+    gulp.src('../src/assets/js/*')
+        .pipe(gulp.dest('app/assets/js'));
+    gulp.src('../src/assets/material-icons/*')
+        .pipe(gulp.dest('app/assets/material-icons'));
+});
+
+
+gulp.task('bower_components', function () {
+    return gulp.src(mainBowerFiles({
+            paths: '../src/',
+            overrides: {
+                angular: {
+                    main: './angular.min.js'
+                },
+                "angular-material": {
+                    main: [
+                        "angular-material.min.css",
+                        "angular-material.min.js"
+                    ]
+                },
+                "angular-material-data-table": {
+                    main: [
+                        "dist/md-data-table.min.js",
+                        "dist/md-data-table.min.css"
+                    ]
+                },
+                "angular-sanitize": {
+                    main: './angular-sanitize.min.js'
+                },
+                "angular-animate": {
+                    main: './angular-animate.min.js'
+                },
+                "angular-ui-router": {
+                    main: './release/angular-ui-router.min.js'
+                },
+                "angular-aria": {
+                    main: './angular-aria.min.js'
+                },
+                "angular-messages": {
+                    main: './angular-messages.min.js'
+                },
+                oclazyload: {
+                    main: './dist/ocLazyLoad.min.js'
+                },
+                moment: {
+                    main: './min/moment.min.js'
+                },
+                jquery: {
+                    main: './dist/jquery.min.js'
+                }
+            }
+        }), {
+            base: '../src/bower_components'
+        })
+        .pipe(gulp.dest('app/bower_components'));
+});
+
+gulp.task('data-dir', function () {
+    gulp.src('../src/data/*.json')
+        .pipe(jsonminify())
+        .pipe(gulp.dest('app/data'));
+});
+
+gulp.task('node_modules', function () {
+    gulp.src('../src/node_modules/**/*', {
+            base: '../src'
+        })
+        .pipe(gulp.dest('app'));
+});
