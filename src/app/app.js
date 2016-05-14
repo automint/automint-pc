2012d92adb3c5b8f571a09ad9021bc57dfa2593f
Anand Kacha <ndkcha@gmail.com>
2016-05-14 11:25:31 +0530

    Changed Debug Configs

--- src/app/app.js
+++ src/app/app.js
@@ -2,7 +2,7 @@
  * Automint Application
  * @author ndkcha
  * @since 0.4.1
- * @version 0.4.1
+ * @version 0.5.0
  */
 
 /// <reference path="../typings/main.d.ts" />
@@ -17,46 +17,53 @@
     angular.module('automintApp', ['ui.router', 'oc.lazyLoad', 'ngSanitize', 'ngAnimate', 'ngMaterial'])
         .config(sceConfig)
         .config(DateFormatConfig)
+        .config(DebugModeConfig)
         .run(RunMainBlock);
-    
+
     sceConfig.$inject = ['$sceDelegateProvider'];
     DateFormatConfig.$inject = ['$mdDateLocaleProvider'];
     RunMainBlock.$inject = ['$rootScope', '$state', '$stateParams', '$window', '$amRoot'];
-    
+    DebugModeConfig.$inject = ['$compileProvider'];
+
     //  configure rules for strict contextual escpaing
     function sceConfig($sceDelegateProvider) {
         $sceDelegateProvider.resourceUrlWhitelist([
             'self'
         ]);
     }
-    
+
     //  configure date formats for the application
     function DateFormatConfig($mdDateLocaleProvider) {
         $mdDateLocaleProvider.formatDate = formatDate;
-        
+
         function parseDate(dateString) {
             return moment(dateString).format('DD/MM/YYYY');
         }
-        
+
         function formatDate(date) {
             return moment(date).format('DD/MM/YYYY');
         }
     }
-    
+
     //  it's called when all the modules are loaded
     function RunMainBlock($rootScope, $state, $stateParams, $window, $amRoot) {
         $rootScope.$state = $state;
         $rootScope.$stateParams = $stateParams;
-        
+
         $rootScope.setCoverPic = setCoverPic;
-        
+
         //  initialize database and default syncing mechanism with automint server
         $amRoot.initDb();
-        
+
         //  set cover photo
         function setCoverPic() {
             var source = localStorage.getItem('cover-pic');
             $('#am-cover-pic').attr('src', (source) ? source : 'assets/img/logo-250x125px.jpg').width(250).height(125);
         }
+    }
+
+    //  configure debug mode
+    function DebugModeConfig($compileProvider) {
+        $compileProvider.debugInfoEnabled(false);
     }
 })();
