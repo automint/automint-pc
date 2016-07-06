ec4be4c4d88dd3d6e78c364eba5dca2037bd3e84
Anand Kacha <ndkcha@gmail.com>
2016-07-06 13:05:12 +0530

    Enhancement #142 : Allow only single instance of the application

--- src/main.js
+++ src/main.js
@@ -35,23 +35,11 @@
     // be closed automatically when the JavaScript object is garbage collected.
     let mainWindow;
 
-    function createWindow() {
-        // Create the browser window.
-        mainWindow = new BrowserWindow({
-            width: 800,
-            height: 600
-        });
-        mainWindow.maximize();
-        // and load the index.html of the app.
-        mainWindow.loadURL('file://' + __dirname + '/index.html');
+    var shouldQuit = app.makeSingleInstance(msiCallback);
 
-        // Emitted when the window is closed.
-        mainWindow.on('closed', function() {
-            // Dereference the window object, usually you would store windows
-            // in an array if your app supports multi windows, this is the time
-            // when you should delete the corresponding element.
-            mainWindow = null;
-        });
+    if (shouldQuit) {
+        app.quit();
+        return;
     }
 
     // This method will be called when Electron has finished
@@ -74,4 +62,31 @@
             createWindow();
         }
     });
+
+    function createWindow() {
+        // Create the browser window.
+        mainWindow = new BrowserWindow({
+            width: 800,
+            height: 600
+        });
+        mainWindow.maximize();
+        // and load the index.html of the app.
+        mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+        // Emitted when the window is closed.
+        mainWindow.on('closed', function() {
+            // Dereference the window object, usually you would store windows
+            // in an array if your app supports multi windows, this is the time
+            // when you should delete the corresponding element.
+            mainWindow = null;
+        });
+    }
+
+    // Someone tried to run a second instance, we should focus our window.
+    function msiCallback(commandLine, workingDirectory) {
+        if (myWindow) {
+            if (myWindow.isMinimized()) myWindow.restore();
+            myWindow.focus();
+        }
+    }
 })();
