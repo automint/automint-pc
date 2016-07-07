8e2fc2deebbb755a18db6b4c25e812292029b779
Anand Kacha <ndkcha@gmail.com>
2016-07-07 17:33:59 +0530

    Bug Fix in #142
    Allow only single instance of the application

--- src/main.js
+++ src/main.js
@@ -84,9 +84,9 @@
 
     // Someone tried to run a second instance, we should focus our window.
     function msiCallback(commandLine, workingDirectory) {
-        if (myWindow) {
-            if (myWindow.isMinimized()) myWindow.restore();
-            myWindow.focus();
+        if (mainWindow) {
+            if (mainWindow.isMinimized()) mainWindow.restore();
+            mainWindow.focus();
         }
     }
 })();
